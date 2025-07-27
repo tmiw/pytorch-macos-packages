bf253a6e39201bd2203a4e7e70e1989c45b558d0
Mooneer Salem <mooneer@gmail.com>
2025-07-27 00:07:01 -0700

    Initial check-in.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PYTORCH_VERSION=v2.7.1
+PYTHON_VERSION=3.12
+
+git clone -b $PYTORCH_VERSION https://github.com/pytorch/pytorch && cd pytorch && git submodule sync && git submodule update --init --recursive
+python$PYTHON_VERSION -m venv torch-venv
+. ./torch-venv/bin/activate
+pip install -r requirements.txt
+BUILD_TEST=0 CMAKE_ONLY=1 CC=clang CXX=clang++ MACOSX_DEPLOYMENT_TARGET=11.0 USE_FBGEMM=0  python$PYTHON_VERSION setup.py bdist_wheel --plat-name macosx_11_0_`uname -m`
