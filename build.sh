48d8d1826e3786c14643d5a4ee442a14958e0f55
Mooneer Salem <mooneer@gmail.com>
2025-08-06 15:45:32 -0700

    Disable MKL due to little performance difference, try mimalloc.

--- build.sh
+++ build.sh
@@ -8,9 +8,7 @@ git clone -b $PYTORCH_VERSION https://github.com/pytorch/pytorch && cd pytorch &
 python$PYTHON_VERSION -m venv torch-venv
 . ./torch-venv/bin/activate
 pip install -r requirements.txt
-if [[ "$SYSTEM_ARCH" == "x86_64" ]]; then
-    pip install mkl mkl-devel mkl-static mkl-include
-    export USE_MKL=1
-    export INTEL_MKL_DIR=$(pwd)/torch-venv
-fi
-CMAKE_POLICY_VERSION_MINIMUM=3.5 MAX_JOBS=$(sysctl -n hw.logicalcpu) CMAKE_OSX_ARCHITECTURES=$SYSTEM_ARCH BUILD_TEST=0 CMAKE_ONLY=1 CC=clang CXX=clang++ MACOSX_DEPLOYMENT_TARGET=11.0 USE_FBGEMM=0  python$PYTHON_VERSION setup.py bdist_wheel --plat-name macosx_11_0_$SYSTEM_ARCH
+#if [[ "$SYSTEM_ARCH" == "x86_64" ]]; then
+#    pip install mkl mkl-devel mkl-static mkl-include
+#fi
+CMAKE_POLICY_VERSION_MINIMUM=3.5 MAX_JOBS=$(sysctl -n hw.logicalcpu) CMAKE_OSX_ARCHITECTURES=$SYSTEM_ARCH BUILD_TEST=0 CMAKE_ONLY=1 CC=clang CXX=clang++ MACOSX_DEPLOYMENT_TARGET=11.0 USE_FBGEMM=0 USE_MIMALLOC=1  python$PYTHON_VERSION setup.py bdist_wheel --plat-name macosx_11_0_$SYSTEM_ARCH
